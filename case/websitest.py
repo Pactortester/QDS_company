05eff5bc97c096016c700a27a2928e233a5d6336
QDS <1456470136@qq.com>
2018-09-05 18:36:28 +0800

    修复bug

--- case/websitest.py
+++ case/websitest.py
@@ -19,14 +19,14 @@ class WebSiTest(MyTestCase):
         time.sleep(2)
         self.driver.find_element_by_css_selector("body > div.footer-wrap > div > ul.items-2 > li:nth-child(7) > a").click()
         print("获取本页所有链接:")
-        url = 'https://pre-www.quandashi.com/site-map/index'
-        r = requests.get(url)
-        r.encoding = 'gb2312'
-
-        # 利用 re （太黄太暴力！）
-        matchs = re.findall(r"(?<=href=\").+?(?=\")|(?<=href=\').+?(?=\')", r.text)
-        for link in matchs:
-            print(link)
+        # url = 'https://pre-www.quandashi.com/site-map/index'
+        # r = requests.get(url)
+        # r.encoding = 'gb2312'
+        #
+        # # 利用 re （太黄太暴力！）
+        # matchs = re.findall(r"(?<=href=\").+?(?=\")|(?<=href=\').+?(?=\')", r.text)
+        # for link in matchs:
+        #     print(link)
         time.sleep(2)
         for link in self.driver.find_elements_by_tag_name("a"):
             print(link.get_attribute("href"))
